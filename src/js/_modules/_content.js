db70cc0b88ee89a1981ade6fef1687ed096b1cc8
MikeO <38439752+mikeOttenhoff@users.noreply.github.com>
2026-01-12 14:01:07 +0100

    scss changes

--- src/js/_modules/_content.js
+++ src/js/_modules/_content.js
@@ -4,22 +4,46 @@ export const content = function () {
   const content = Object.entries(content_database);
   const content_container = document.querySelector(".content_container");
 
+  function appendLink(container, href, text) {
+    if (!href) return;
+
+    const link = document.createElement("a");
+    link.href = href;
+    link.textContent = text;
+    container.append(link);
+  }
+
   content.forEach(function ([key, item]) {
     const assignment_container = document.createElement("div");
-    const assignment_title = item.title;
-    const assignment_textContent = item.textContent;
-    const assignment_img_link = item.img;
-    const assignment_link = item.link;
-    const assignment_github_link = item.github_link;
+    assignment_container.classList.add("content");
+
+    const assignment_img_link = document.createElement("img");
+    assignment_img_link.src = item.img;
+
+    if (item.img) {
+      assignment_container.append(assignment_img_link);
+    }
+
+    const assignment_title = document.createElement("h3");
+    assignment_title.textContent = item.title;
+    assignment_title.classList.add("content_title");
+
+    const assignment_textContent = document.createElement("p");
+    assignment_textContent.textContent = item.textContent;
+
+    const assignment_linkContainer = document.createElement("div");
+    assignment_linkContainer.classList.add("link_container");
+
+    appendLink(assignment_linkContainer, item.link, item.link_name);
+    appendLink(assignment_linkContainer, item.github_link, "GitHub");
+
     assignment_container.append(
       assignment_title,
       assignment_textContent,
-      assignment_img_link,
-      assignment_link,
-      assignment_github_link
+      assignment_linkContainer
     );
 
     content_container.append(assignment_container);
-    console.log(key, item);
+    // console.log(key, item);
   });
 };
--- src/js/_modules/_content_database.js
+++ src/js/_modules/_content_database.js
@@ -1,20 +1,34 @@
+import logo from "../../img/Logo.svg";
+
 export const content_database = {
   ding1: {
-    title: "ding1",
+    title: "Hello World!",
     textContent:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam aliquam enim nec turpis tempor molestie. ",
-    img: "",
-    link: "",
-    github_link: "",
+    img: logo,
+    link: "https://developer.mozilla.org/en-US/",
+    link_name: "MDN",
+    github_link: "https://github.com/mikeOttenhoff/hello-world",
     keyWords: ["grafisch vormgeven", "motion graphics"],
   },
   ding2: {
-    title: "ding2",
+    title: "Git test",
+    textContent:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam aliquam enim nec turpis tempor molestie. ",
+    img: "",
+    link: "https://stackoverflow.com/questions",
+    link_name: "Stack Overflow",
+    github_link: "https://github.com/mikeOttenhoff/git_test",
+    keyWords: ["Git"],
+  },
+  ding3: {
+    title: "ding3",
     textContent:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam aliquam enim nec turpis tempor molestie. ",
     img: "",
     link: "",
+    link_name: "",
     github_link: "",
-    keyWords: ["grafisch vormgeven"],
+    keyWords: ["grafisch vormgeven, design"],
   },
 };
