4d1b9951b3c0708dff353733ab9d8267d9ba44a6
MikeO <38439752+mikeOttenhoff@users.noreply.github.com>
2026-02-23 15:16:11 +0100

    removed pages from public

--- webpack.config.js
+++ webpack.config.js
@@ -10,122 +10,84 @@ const pageFiles = fs.existsSync(pagesDir)
   ? fs.readdirSync(pagesDir).filter(f => f.endsWith(".html"))
   : [];
 
-module.exports = (env, argv) => {
-  const isProduction = argv.mode === "production";
+module.exports = {
+  mode: "production",
+  entry: "./src/js/main.js", // Make sure this file exists
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/main.[contenthash].js",
+    clean: true,
+    publicPath: "./",
+  },
+  plugins: [
+    // Main index.html
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
 
-  return {
-    mode: isProduction ? "production" : "development",
-    devtool: isProduction ? false : "inline-source-map",
+    // Auto-generate pages from src/pages
+    ...pageFiles.map(
+      file =>
+        new HtmlWebpackPlugin({
+          template: path.join(pagesDir, file),
+          filename: `pages/${file}`,
+          title: file.replace(".html", ""),
+        }),
+    ),
 
-    entry: "./src/js/main.js",
+    // Extract CSS
+    new MiniCssExtractPlugin({
+      filename: "css/main.[contenthash].css",
+    }),
 
-    output: {
-      filename: isProduction ? "js/main.[contenthash].js" : "js/main.js",
-      path: path.resolve(__dirname, "dist"),
-      clean: true,
-      publicPath: "./",
-    },
-
-    devServer: isProduction
-      ? undefined
-      : {
-          static: {
-            directory: path.join(__dirname, "public"), // serve static files from public
-            publicPath: "/",
-          },
-          watchFiles: [
-            "src/**/*", // watch all source files (JS, CSS, templates)
-            "src/pages/**/*.html", // watch page templates
-          ],
-          devMiddleware: {
-            writeToDisk: true, // write files to disk (optional)
-          },
-          open: true,
-          hot: false,
-          liveReload: true,
-          historyApiFallback: false, // serve static files normally
-        },
-
-    plugins: [
-      // Main index.html
-      new HtmlWebpackPlugin({
-        template: "./src/index.html",
-        filename: "index.html",
-        title: "Mike Ottenhoff, audiovisual vormgever",
-        favicon: "./src/img/mo_favicon.svg",
-      }),
-
-      // Auto-generate HtmlWebpackPlugin for each page in src/pages
-      ...pageFiles.map(
-        file =>
-          new HtmlWebpackPlugin({
-            template: path.join(pagesDir, file),
-            filename: `pages/${file}`, // output into dist/pages/
-            title: file.replace(".html", ""),
-          }),
-      ),
-
-      // Extract CSS into a separate file
-      new MiniCssExtractPlugin({
-        filename: isProduction ? "css/main.[contenthash].css" : "css/main.css",
-      }),
-
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: path.resolve(__dirname, "public"),
-            to: "", // copy into dist root
-            noErrorOnMissing: true,
-            globOptions: {
-              dot: true,
-              gitignore: true,
-            },
-          },
-        ],
-      }),
-    ],
-
-    module: {
-      rules: [
-        // CSS: extract to separate file
+    // Copy everything from public into dist
+    new CopyWebpackPlugin({
+      patterns: [
         {
-          test: /\.css$/i,
-          use: [MiniCssExtractPlugin.loader, "css-loader"],
-        },
-
-        // Images & media files
-        {
-          test: /\.(png|svg|jpg|jpeg|gif|webp|webm)$/i,
-          type: "asset/resource",
-          generator: {
-            filename: "img/[name][ext]", // copy all images to dist/img
+          from: path.resolve(__dirname, "public"),
+          to: "", // copy into dist root
+          noErrorOnMissing: true,
+          globOptions: {
+            dot: true,
+            gitignore: true,
+            ignore: ["pages/*.html"],
           },
         },
-
-        // Canva HTML exports in src/img
-        {
-          test: /\.html$/,
-          include: path.resolve(__dirname, "src/img"),
-          type: "asset/resource",
-          generator: {
-            filename: "img/[name][ext]", // copy to dist/img without parsing
-          },
-        },
-
-        // // Static HTML from public (excluding src/img)
-        // {
-        //   test: /\.html$/,
-        //   include: path.resolve(__dirname, "public"),
-        //   exclude: path.resolve(__dirname, "src/img"),
-        //   type: "asset/resource",
-        // },
       ],
-    },
-
-    resolve: {
-      alias: {
-        "@img": path.resolve(__dirname, "src/img"),
+    }),
+  ],
+  module: {
+    rules: [
+      // CSS
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
+
+      // Images & media
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|webp|webm)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "img/[name][ext]",
+        },
+      },
+
+      // HTML exports inside src/img (like Canva exports)
+      {
+        test: /\.html$/,
+        include: path.resolve(__dirname, "src/img"),
+        type: "asset/resource",
+        generator: {
+          filename: "img/[name][ext]",
+        },
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      "@img": path.resolve(__dirname, "src/img"),
     },
-  };
+  },
 };
